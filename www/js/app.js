8c5a98ca43643697f2d7db756afe8c3278df00e0
tharindu <tharindu.prf@gmail.com>
2015-09-30 17:08:49 +0530

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,68 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('mobile', ['ionic', 'mobile.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    abstract:true,
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+    .state('app.item', {
+      url: '/item/:item',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/item.html',
+          controller: 'ItemCtrl'
+        }
+      }
+      
+    })
+
+  
+  .state('app.home', {
+      url: '/home',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/home.html',
+       //   controller: 'HomeCtrl'
+        }
+      }
+    })
+
+  .state('app.home.social', {
+    url: '/app/home/social',
+    views: {
+      'tab-social': {
+        templateUrl: 'templates/social.html',
+        
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
