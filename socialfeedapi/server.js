b616ee496854316a4a62b58645db5ff9a0bb6a08
tharindu <tharindu.prf@gmail.com>
2015-11-26 14:37:59 +0530

    fdgfdg

--- socialfeedapi/server.js
+++ socialfeedapi/server.js
@@ -16,7 +16,11 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.use('/api',require('./routes/api'))
 
